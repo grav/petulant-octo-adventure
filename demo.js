97af315452e3bf8179f22757f9a71648f3271d5f
Mikkel Gravgaard <mikkel@klokke.dk>
2013-11-02 12:44:52 +0100

    no connections to start with

--- demo.js
+++ demo.js
@@ -7,30 +7,32 @@ canvasEl.height = $(canvasEl).height();
 var ctx      = canvasEl.getContext( '2d' );
 
 var g = new Graph(ctx);
-g.make('OUT', context.destination);
+// g.make('OUT', context.destination);
 
 var pet = new PUA(context);
 var tuna = new Tuna(context, pet);
 tuna.eventHandler = function(event) { g.swallow(event) };
 pet.eventHandler = function(event) { g.swallow(event) };
 
-g.make('sc', new pet.SoundCloud('https://soundcloud.com/lordemusic/royals'));
+g.start();
 
-g.make('speaker', new pet.Speaker(meSpeak));
-g.speaker.visual = new pet.Waveform();
-g.speaker.connect(context.destination);
-g.sc.commentsEmitter.connect(g.speaker);
-
-g.make('chorus', new tuna.Chorus({
-                 rate: 10.5,
-                 feedback: 0.2,
-                 delay: 0.0045,
-                 bypass: 0
-             }));
-g.chorus.visual = new pet.Waveform();
-
-g.sc.connect(g.chorus);
-g.chorus.connect(context.destination);
+// g.make('sc', new pet.SoundCloud('https://soundcloud.com/lordemusic/royals'));
+// 
+// g.make('speaker', new pet.Speaker(meSpeak));
+// g.speaker.visual = new pet.Waveform();
+// g.speaker.connect(context.destination);
+// g.sc.commentsEmitter.connect(g.speaker);
+// 
+// g.make('chorus', new tuna.Chorus({
+//                  rate: 10.5,
+//                  feedback: 0.2,
+//                  delay: 0.0045,
+//                  bypass: 0
+//              }));
+// g.chorus.visual = new pet.Waveform();
+// 
+// g.sc.connect(g.chorus);
+// g.chorus.connect(context.destination);
 
 //g.e.play()
 //g.e.pause()
