c3a44e36764a311893f7cf0de4a6f3d1ac7dd2d0
Mikkel Gravgaard <mikkel@klokke.dk>
2013-11-02 12:52:13 +0100

    more demo friendly

--- demo.js
+++ demo.js
@@ -7,6 +7,9 @@ canvasEl.height = $(canvasEl).height();
 var ctx      = canvasEl.getContext( '2d' );
 
 var g = new Graph(ctx);
+g.marginX = 30;
+g.marginY = 30;
+
 g.make('OUT', context.destination);
 
 var pet = new PUA(context);
@@ -14,23 +17,24 @@ var tuna = new Tuna(context, pet);
 tuna.eventHandler = function(event) { g.swallow(event) };
 pet.eventHandler = function(event) { g.swallow(event) };
 
-g.make('sc', new pet.SoundCloud('https://soundcloud.com/lordemusic/royals'));
+g.make('soundCloud', new pet.SoundCloud('https://soundcloud.com/lordemusic/royals'));
 
-g.make('speaker', new pet.Speaker(meSpeak));
-g.speaker.visual = new pet.Waveform();
-g.speaker.connect(context.destination);
-g.sc.commentsEmitter.connect(g.speaker);
+g.make('speech', new pet.Speaker(meSpeak));
+g.speech.visual = new pet.Waveform();
+// g.speaker.connect(context.destination);
+// g.soundCloud.commentsEmitter.connect(g.speaker);
 
-g.make('chorus', new tuna.Chorus({
-                 rate: 10.5,
-                 feedback: 0.2,
-                 delay: 0.0045,
+g.make('phaser', new tuna.Phaser({
+                 rate: 1.2,                     //0.01 to 8 is a decent range, but higher values are possible
+                 depth: 0.7,                    //0 to 1
+                 feedback: 0.2,                 //0 to 1+
+                 stereoPhase: 30,               //0 to 180
+                 baseModulationFrequency: 700,  //500 to 1500
                  bypass: 0
              }));
-g.chorus.visual = new pet.Waveform();
 
-g.sc.connect(g.chorus);
-g.chorus.connect(context.destination);
+// g.sc.connect(g.chorus);
+// g.chorus.connect(context.destination);
 
 //g.e.play()
 //g.e.pause()
